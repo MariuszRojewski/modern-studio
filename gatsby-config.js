908e98ceb18e549eeea995bd70aac17112be2b94
tytan-reklama <157692986+tytan-reklama@users.noreply.github.com>
2024-07-29 15:30:29 +0200

    initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,79 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const path = require('path');
+// Get paths of Gatsby's required rules, which as of writing is located at:
+// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+// gatsby/src/utils/eslint-rules
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  'node_modules',
+  'gatsby',
+  'dist',
+  'utils',
+  'eslint-rules'
+);
+
 module.exports = {
   siteMetadata: {
     title: `moder-studio`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "0844JNrtarNJvm7zrhlSKW6iewzlHMIpZDw7i_mWx80",
-      "spaceId": "6izushv8kxhj"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`montserrat\:300,500,700`, `cormorant garamond\:500`],
+        display: 'swap',
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        assets: path.join(__dirname, 'src', 'assets'),
+        components: path.join(__dirname, 'src', 'components'),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be omitted or customized
+        stages: ['develop'],
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        exclude: ['node_modules', 'bower_components', '.cache', 'public'],
+        // Any additional eslint-webpack-plugin options below
+        // ...
+      },
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: '0844JNrtarNJvm7zrhlSKW6iewzlHMIpZDw7i_mWx80',
+        spaceId: '6izushv8kxhj',
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sitemap',
+    // "gatsby-plugin-google-gtag"
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/assets/images/icon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/assets/images/',
+      },
+      __key: 'images',
+    },
+  ],
 };
